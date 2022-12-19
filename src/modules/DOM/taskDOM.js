fca81d6ceb0146dbbf8d9fd73bd2acdc2b0997ac
Rodesar <rockoesp@hotmail.com>
2022-12-19 11:38:39 -0600

    Use named function expressions Only applied to non imported functions

--- src/modules/DOM/taskDOM.js
+++ src/modules/DOM/taskDOM.js
@@ -2,48 +2,41 @@ import taskLogic from '../app_logic/taskLogic';
 import projectList from '../app_logic/projectList';
 import dynamicClick from './dynamicClick';
 
-export default function displayTasks(priority) {
-    const taskList = document.querySelector('.tasks');
-    if (priority) { taskList.textContent = ''; }
-
-    const tasks = taskLogic.getTasksForCurrentProject(priority);
-
-    tasks.forEach((taskObj) => {
-        taskList.appendChild(createTaskElement(taskObj, priority));
-    });
-
-    if (priority) { displayTasks(false); }
-}
-
-function createTaskElement(obj, priority) {
-    const task = createTaskDiv(priority);
-    const checkmark = createCheckmark(task, obj);
-    const title = createTitle(obj);
-    const description = createDescription(obj);
-    const date = createDate(obj);
-    const editBtn = createEditButton(obj, task);
-    const deleteBtn = createDeleteButton(obj);
-
-    task.appendChild(checkmark);
-    task.appendChild(title);
-    title.appendChild(taskIsFromThisProject(obj));
-    task.appendChild(description);
-    task.appendChild(date);
-    task.appendChild(editBtn);
-    task.appendChild(deleteBtn);
-
-    return task;
-}
-
-function createTaskDiv(priority) {
+const createTaskDiv = function createTaskDiv(priority) {
     const task = document.createElement('div');
     task.classList.add('task');
     if (priority) { task.classList.add('high-priority-task'); }
 
     return task;
-}
+};
 
-function createCheckmark(task, obj) {
+const displayCheckmark = function displayCheckmark(checkmark, task) {
+    if (checkmark.textContent) {
+        checkmark.textContent = '';
+        task.classList.remove('completed');
+    } else {
+        checkmark.textContent = '✔';
+        task.classList.add('completed');
+    }
+};
+
+const checkmarkClick = function checkmarkClick(checkmark, obj, task) {
+    displayCheckmark(checkmark, task);
+
+    taskLogic.updateTaskStatus(obj);
+
+    dynamicClick.dynamicShow(['newProjectBtn']);
+    dynamicClick.dynamicHide(['taskForm', 'projectForm']);
+};
+
+const checkTaskCompletion = function checkTaskCompletion(checkmark, obj, task) {
+    if (obj.isCompleted) {
+        checkmark.textContent = '✔';
+        task.classList.add('completed');
+    }
+};
+
+const createCheckmark = function createCheckmark(task, obj) {
     const checkmark = document.createElement('div');
     checkmark.classList.add('checkmark');
     checkmark.addEventListener(
@@ -54,105 +47,108 @@ function createCheckmark(task, obj) {
     checkTaskCompletion(checkmark, obj, task);
 
     return checkmark;
-}
+};
 
-function checkTaskCompletion(checkmark, obj, task) {
-    if (obj.isCompleted) {
-        checkmark.textContent = '✔';
-        task.classList.add('completed');
-    }
-}
-
-function checkmarkClick(checkmark, obj, task) {
-    displayCheckmark(checkmark, task);
-
-    taskLogic.updateTaskStatus(obj);
-
-    dynamicClick.dynamicShow(['newProjectBtn']);
-    dynamicClick.dynamicHide(['taskForm', 'projectForm']);
-}
-
-function displayCheckmark(checkmark, task) {
-    if (checkmark.textContent) {
-        checkmark.textContent = '';
-        task.classList.remove('completed');
-    } else {
-        checkmark.textContent = '✔';
-        task.classList.add('completed');
-    }
-}
-
-function createTitle(obj) {
+const createTitle = function createTitle(obj) {
     const title = document.createElement('h3');
     title.textContent = obj.title;
     title.classList.add('task-title');
 
     return title;
-}
+};
 
-function createDescription(obj) {
+const createDescription = function createDescription(obj) {
     const description = document.createElement('p');
     description.textContent = obj.description;
     description.classList.add('task-description');
 
     return description;
-}
+};
 
-function createDate(obj) {
+const createDate = function createDate(obj) {
     const date = document.createElement('p');
     date.textContent = obj.date;
     date.classList.add('task-date');
 
     return date;
-}
+};
 
-function createEditButton(obj, task) {
-    const editBtn = document.createElement('i');
-    editBtn.classList.add('fa-solid');
-    editBtn.classList.add('fa-pen-to-square');
-    editBtn.classList.add('edit-task-button');
-    editBtn.addEventListener(
-        'click',
-        () => editClick(obj, task, editBtn),
-    );
-
-    return editBtn;
-}
-
-function createDeleteButton(obj) {
-    const deleteBtn = document.createElement('i');
-    deleteBtn.classList.add('fa-regular');
-    deleteBtn.classList.add('fa-trash-can');
-    deleteBtn.classList.add('delete-task-button');
-    deleteBtn.addEventListener('click', () => deleteClick(obj));
-
-    return deleteBtn;
-}
-
-function deleteClick(obj) {
-    taskLogic.deleteTask(obj);
+const submitEdit = function submitEdit(obj, editForm) {
+    taskLogic.submitEdit(obj, editForm);
     displayTasks(true);
+};
 
-    dynamicClick.dynamicShow(['newProjectBtn']);
-    dynamicClick.dynamicHide(['taskForm', 'projectForm']);
-}
+const createEditFormElement = function createEditFormElement(obj) {
+    const editForm = document.createElement('form');
+    editForm.classList.add('edit-form');
+    editForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        submitEdit(obj, editForm);
+    });
 
-function editClick(obj, task, editBtn) {
-    createEditForm(obj, task, editBtn);
+    return editForm;
+};
 
-    clearTaskElements(editBtn);
+const getCurrentTaskElement = function getCurrentTaskElement(childElement) {
+    const taskDiv = childElement.parentElement;
 
-    dynamicClick.dynamicHide(['taskForm', 'projectForm']);
-    dynamicClick.dynamicShow(['newProjectBtn']);
-}
+    const title = taskDiv.querySelector('.task-title');
+    const description = taskDiv.querySelector('.task-description');
+    const date = taskDiv.querySelector('.task-date');
+    const deleteBtn = taskDiv.querySelector('.delete-task-button');
 
-function clearTaskElements(editBtn) {
-    getCurrentTaskElement(editBtn).description.remove();
-    getCurrentTaskElement(editBtn).date.remove();
-    editBtn.remove();
-}
+    return {
+        title, description, date, deleteBtn,
+    };
+};
 
-function createEditForm(obj, task, editBtn) {
+const createDescriptionInput = function createDescriptionInput(editBtn) {
+    const descInp = document.createElement('input');
+    descInp.classList.add('edit-description-input');
+    descInp.setAttribute('placeholder', 'Description');
+    descInp.value = getCurrentTaskElement(editBtn).description.textContent;
+
+    return descInp;
+};
+
+const createDateInput = function createDateInput(editBtn) {
+    const dateInp = document.createElement('input');
+    dateInp.type = 'date';
+    dateInp.classList.add('edit-date-input');
+    dateInp.value = getCurrentTaskElement(editBtn).date.textContent;
+
+    return dateInp;
+};
+
+const createPriorityLabel = function createPriorityLabel() {
+    const priorityLabel = document.createElement('label');
+    priorityLabel.textContent = 'Mark High Priority';
+
+    return priorityLabel;
+};
+
+const createPriorityInput = function createPriorityInput(obj) {
+    const priorityInp = document.createElement('input');
+    priorityInp.classList.add('edit-priority-input');
+    priorityInp.type = 'checkbox';
+    priorityInp.checked = obj.priority;
+
+    return priorityInp;
+};
+
+const createAcceptButton = function createAcceptButton() {
+    const accept = document.createElement('button');
+
+    const icon = document.createElement('i');
+    icon.classList.add('fa-solid');
+    icon.classList.add('fa-circle-check');
+
+    accept.appendChild(icon);
+
+    return accept;
+};
+
+const createEditForm = function createEditForm(obj, task, editBtn) {
     const editForm = createEditFormElement(obj);
     const descriptionInput = createDescriptionInput(editBtn);
     const dateInput = createDateInput(editBtn);
@@ -168,84 +164,55 @@ function createEditForm(obj, task, editBtn) {
     editForm.appendChild(acceptChanges);
 
     task.insertBefore(editForm, getCurrentTaskElement(editBtn).deleteBtn);
-}
+};
 
-function createEditFormElement(obj) {
-    const editForm = document.createElement('form');
-    editForm.classList.add('edit-form');
-    editForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        submitEdit(obj, editForm);
-    });
+const clearTaskElements = function clearTaskElements(editBtn) {
+    getCurrentTaskElement(editBtn).description.remove();
+    getCurrentTaskElement(editBtn).date.remove();
+    editBtn.remove();
+};
 
-    return editForm;
-}
+const editClick = function editClick(obj, task, editBtn) {
+    createEditForm(obj, task, editBtn);
 
-function submitEdit(obj, editForm) {
-    taskLogic.submitEdit(obj, editForm);
+    clearTaskElements(editBtn);
+
+    dynamicClick.dynamicHide(['taskForm', 'projectForm']);
+    dynamicClick.dynamicShow(['newProjectBtn']);
+};
+
+const createEditButton = function createEditButton(obj, task) {
+    const editBtn = document.createElement('i');
+    editBtn.classList.add('fa-solid');
+    editBtn.classList.add('fa-pen-to-square');
+    editBtn.classList.add('edit-task-button');
+    editBtn.addEventListener(
+        'click',
+        () => editClick(obj, task, editBtn),
+    );
+
+    return editBtn;
+};
+
+const deleteClick = function deleteClick(obj) {
+    taskLogic.deleteTask(obj);
     displayTasks(true);
-}
 
-function createDescriptionInput(editBtn) {
-    const descInp = document.createElement('input');
-    descInp.classList.add('edit-description-input');
-    descInp.setAttribute('placeholder', 'Description');
-    descInp.value = getCurrentTaskElement(editBtn).description.textContent;
+    dynamicClick.dynamicShow(['newProjectBtn']);
+    dynamicClick.dynamicHide(['taskForm', 'projectForm']);
+};
 
-    return descInp;
-}
+const createDeleteButton = function createDeleteButton(obj) {
+    const deleteBtn = document.createElement('i');
+    deleteBtn.classList.add('fa-regular');
+    deleteBtn.classList.add('fa-trash-can');
+    deleteBtn.classList.add('delete-task-button');
+    deleteBtn.addEventListener('click', () => deleteClick(obj));
 
-function createDateInput(editBtn) {
-    const dateInp = document.createElement('input');
-    dateInp.type = 'date';
-    dateInp.classList.add('edit-date-input');
-    dateInp.value = getCurrentTaskElement(editBtn).date.textContent;
+    return deleteBtn;
+};
 
-    return dateInp;
-}
-
-function createPriorityLabel() {
-    const priorityLabel = document.createElement('label');
-    priorityLabel.textContent = 'Mark High Priority';
-
-    return priorityLabel;
-}
-
-function createPriorityInput(obj) {
-    const priorityInp = document.createElement('input');
-    priorityInp.classList.add('edit-priority-input');
-    priorityInp.type = 'checkbox';
-    priorityInp.checked = obj.priority;
-
-    return priorityInp;
-}
-
-function createAcceptButton() {
-    const accept = document.createElement('button');
-
-    const icon = document.createElement('i');
-    icon.classList.add('fa-solid');
-    icon.classList.add('fa-circle-check');
-
-    accept.appendChild(icon);
-
-    return accept;
-}
-
-function getCurrentTaskElement(childElement) {
-    const taskDiv = childElement.parentElement;
-
-    const title = taskDiv.querySelector('.task-title');
-    const description = taskDiv.querySelector('.task-description');
-    const date = taskDiv.querySelector('.task-date');
-    const deleteBtn = taskDiv.querySelector('.delete-task-button');
-
-    return {
-        title, description, date, deleteBtn,
-    };
-}
-
-function taskIsFromThisProject(obj) {
+const taskIsFromThisProject = function taskIsFromThisProject(obj) {
     const projectName = document.createElement('span');
 
     if (!projectList.getCurrentProject().title) {
@@ -263,4 +230,37 @@ function taskIsFromThisProject(obj) {
     }
 
     return projectName;
+};
+
+const createTaskElement = function createTaskElement(obj, priority) {
+    const task = createTaskDiv(priority);
+    const checkmark = createCheckmark(task, obj);
+    const title = createTitle(obj);
+    const description = createDescription(obj);
+    const date = createDate(obj);
+    const editBtn = createEditButton(obj, task);
+    const deleteBtn = createDeleteButton(obj);
+
+    task.appendChild(checkmark);
+    task.appendChild(title);
+    title.appendChild(taskIsFromThisProject(obj));
+    task.appendChild(description);
+    task.appendChild(date);
+    task.appendChild(editBtn);
+    task.appendChild(deleteBtn);
+
+    return task;
+};
+
+export default function displayTasks(priority) {
+    const taskList = document.querySelector('.tasks');
+    if (priority) { taskList.textContent = ''; }
+
+    const tasks = taskLogic.getTasksForCurrentProject(priority);
+
+    tasks.forEach((taskObj) => {
+        taskList.appendChild(createTaskElement(taskObj, priority));
+    });
+
+    if (priority) { displayTasks(false); }
 }
--- src/modules/app_logic/storage.js
+++ src/modules/app_logic/storage.js
@@ -1,34 +1,12 @@
 import Project from './project';
 import projectList from './projectList';
 
-export function updateStorage() {
-    localStorage.setItem('projects', JSON.stringify(projectList.getProjects()));
-}
-
-export function checkStorage() {
-    if (localStorage.length) {
-        loadProjectsFromStorage();
-    } else {
-        createHomeProject();
-    }
-
-    projectList.setCurrentProject(projectList.getHomeProject());
-}
-
-function createHomeProject() {
+const init = function createHomeProject() {
     const home = new Project('Home');
     projectList.addProject(home);
-}
+};
 
-function loadProjectsFromStorage() {
-    const projects = JSON.parse(localStorage.getItem('projects'));
-
-    projects.forEach((project) => {
-        addProjectToList(project);
-    });
-}
-
-function addProjectToList(project) {
+const add = function addProjectToList(project) {
     const newProject = new Project(project.title);
     newProject.tasks = project.tasks;
     newProject.priorityTasks = project.priorityTasks;
@@ -36,4 +14,26 @@ function addProjectToList(project) {
     projectList.addProject(newProject);
 
     return newProject;
+};
+
+const load = function loadProjectsFromStorage() {
+    const projects = JSON.parse(localStorage.getItem('projects'));
+
+    projects.forEach((project) => {
+        add(project);
+    });
+};
+
+export function updateStorage() {
+    localStorage.setItem('projects', JSON.stringify(projectList.getProjects()));
+}
+
+export function checkStorage() {
+    if (localStorage.length) {
+        load();
+    } else {
+        init();
+    }
+
+    projectList.setCurrentProject(projectList.getHomeProject());
 }
